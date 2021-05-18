c760cfd142535d6a2a31a76693e700277d6418a5
菘菘 <rrt467778@gmail.com>
2021-05-18 19:06:40 +0800

    test

--- Spider/modpack.js
+++ Spider/modpack.js
@@ -12,9 +12,9 @@ const {
     stringify,
 } = require('comment-json')
 
-let dirPath = path.join(__dirname, "mod");
-if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
+let ModDirPath = path.join(__dirname, "mod");
+if (!fs.existsSync(ModDirPath)) {
+    fs.mkdirSync(ModDirPath);
 }
 if (!fs.existsSync(path.join(__dirname, "../assets"))) {
     fs.mkdirSync(path.join(__dirname, "../assets"));
@@ -32,20 +32,20 @@ for (let k = 0; k < 1; k++) {
 
     function aaa() {
         for (let i = 0; i < ModList.length; i++) {
-            let slug = ModList[i].projectID;
-
+            let slug;
             let fileID;
-            let fileName
+            let fileName;
             curseforge.getModFiles(ModList[i].projectID).then((files) => {
                 for (let i = 0; i < files.length; i++) {
                     if (files[i].minecraft_versions.includes(config.ver)) {
                         fileID = String(files[i].id)
                         fileName = String(files[i].download_url.split("https://edge.forgecdn.net/files/")[1].split(`${fileID.substr(0, 4)}/${fileID.substr(4, 7)}/`)[1]);
-                       let test = path.join(dirPath, fileName);
+                        let test = path.join(ModDirPath, fileName);
+                        slug = fileName.split(".jar")[1];
                         try {
-                            files[i].download(test,true).then(r => {
+                            files[i].download(test, true).then(r => {
                                 console.log(`${fileName} 下載完成。`)
-                                compressing.zip.uncompress(`./mod/${fileName}`, "../jar/" + slug).then(() => compressing_done())
+                                compressing.zip.uncompress(`./mod/${fileName}`, "../jar/" + slug).then(() => compressing_done(slug))
                             });
                         } catch (err) {
                             console.log("發生未知錯誤 \n" + err)
@@ -54,67 +54,66 @@ for (let k = 0; k < 1; k++) {
                     }
                 }
             });
-
-            function compressing_done() {
-                let mod_id;
-                let dirPath_2 = path.join(__dirname, `../jar/${slug}/META-INF/mods.toml`);
-                let dirPath_3 = path.join(__dirname, `../jar/${slug}/mcmod.info`);
-                let dirPath_6 = path.join(__dirname, `../jar/${slug}/fabric.mod.json`);
-                if (fs.existsSync(dirPath_2)) {
-                    fs.createReadStream(`../jar/${slug}/META-INF/mods.toml`, 'utf8').pipe(concat(function (data, err) {
-                        try {
-                            mod_id = JSON.parse(JSON.stringify(toml.parse(data))).mods[0].modId
-                            mod_assets(mod_id);
-                        } catch (err) {
-                            console.log("解析Toml時發生錯誤 \n" + err)
-                        }
-                    }));
-                } else if (fs.existsSync(dirPath_3)) {
-                    fs.createReadStream(`../jar/${slug}/mcmod.info`, 'utf8').pipe(concat(function (data, err) {
-                        try {
-                            mod_id = JSON.parse(data)[0].modid;
-                            mod_assets(mod_id);
-                        } catch (err) {
-                            console.log("解析Json時發生錯誤 \n" + err)
-                        }
-                    }));
-                } else if (fs.existsSync(dirPath_6)) {
-                    fs.createReadStream(`../jar/${slug}/fabric.mod.json`, 'utf8').pipe(concat(function (data, err) {
-                        try {
-                            mod_id = JSON.parse(data).id;
-                            mod_assets(mod_id);
-                        } catch (err) {
-                            console.log("解析Json時發生錯誤 \n" + err)
-                        }
-                    }));
-                }
-
-
-                function mod_assets(mod_id) {
-                    let dirPath_4 = path.join(__dirname, "../jar/" + slug + "/assets/" + mod_id + "/lang/en_us.json");
-                    if (!fs.existsSync(dirPath_4)) return
-
-                    let dirPath_5 = path.join(__dirname, "../assets/" + mod_id);
-                    if (!fs.existsSync(dirPath_5)) {
-                        fs.mkdirSync(dirPath_5);
-                    }
-                    let dirPath_1 = path.join(__dirname, "../assets/" + mod_id + "/lang");
-                    if (!fs.existsSync(dirPath_1)) {
-                        fs.mkdirSync(dirPath_1);
-                    }
-
-                    let data = fs.readFileSync("../jar/" + slug + "/assets/" + mod_id + "/lang/en_us.json").toString();
-                    data = parse(data, null, true)
-                    data = stringify(data, null, 4)
-
-                    fs.writeFile("../assets/" + mod_id + "/lang/zh_tw.json", data, function (error) {
-                        console.log(`處理 ${fileName} (${mod_id}) 的原始語系檔案完成`)
-                        if (error) {
-                            console.log(`解析語系Json檔案時發生錯誤。\n錯誤模組檔案: ${fileName} (${mod_id})\n錯誤原因: ${error}`);
-                        }
-                    })
-                }
-            }
         }
     }
+}
+
+function compressing_done(slug) {
+    let mod_id;
+    let dirPath_2 = path.join(__dirname, `../jar/${slug}/META-INF/mods.toml`);
+    let dirPath_3 = path.join(__dirname, `../jar/${slug}/mcmod.info`);
+    let dirPath_6 = path.join(__dirname, `../jar/${slug}/fabric.mod.json`);
+    if (fs.existsSync(dirPath_2)) {
+        fs.createReadStream(`../jar/${slug}/META-INF/mods.toml`, 'utf8').pipe(concat(function (data, err) {
+            try {
+                mod_id = JSON.parse(JSON.stringify(toml.parse(data))).mods[0].modId
+                ModAssets(mod_id);
+            } catch (err) {
+                console.log("解析Toml時發生錯誤 \n" + err)
+            }
+        }));
+    } else if (fs.existsSync(dirPath_3)) {
+        fs.createReadStream(`../jar/${slug}/mcmod.info`, 'utf8').pipe(concat(function (data, err) {
+            try {
+                mod_id = JSON.parse(data)[0].modid;
+                ModAssets(mod_id);
+            } catch (err) {
+                console.log("解析Json時發生錯誤 \n" + err)
+            }
+        }));
+    } else if (fs.existsSync(dirPath_6)) {
+        fs.createReadStream(`../jar/${slug}/fabric.mod.json`, 'utf8').pipe(concat(function (data, err) {
+            try {
+                mod_id = JSON.parse(data).id;
+                ModAssets(mod_id);
+            } catch (err) {
+                console.log("解析Json時發生錯誤 \n" + err)
+            }
+        }));
+    }
+}
+
+function ModAssets(mod_id) {
+    let dirPath_4 = path.join(__dirname, "../jar/" + slug + "/assets/" + mod_id + "/lang/en_us.json");
+    if (!fs.existsSync(dirPath_4)) return
+
+    let dirPath_5 = path.join(__dirname, "../assets/" + mod_id);
+    if (!fs.existsSync(dirPath_5)) {
+        fs.mkdirSync(dirPath_5);
+    }
+    let dirPath_1 = path.join(__dirname, "../assets/" + mod_id + "/lang");
+    if (!fs.existsSync(dirPath_1)) {
+        fs.mkdirSync(dirPath_1);
+    }
+
+    let data = fs.readFileSync("../jar/" + slug + "/assets/" + mod_id + "/lang/en_us.json").toString();
+    data = parse(data, null, true)
+    data = stringify(data, null, 4)
+
+    fs.writeFile("../assets/" + mod_id + "/lang/zh_tw.json", data, function (error) {
+        console.log(`處理 ${fileName} (${mod_id}) 的原始語系檔案完成`)
+        if (error) {
+            console.log(`解析語系Json檔案時發生錯誤。\n錯誤模組檔案: ${fileName} (${mod_id})\n錯誤原因: ${error}`);
+        }
+    })
 }
