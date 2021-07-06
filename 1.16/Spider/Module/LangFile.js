a1c329c4734f12b52778017f5f47defb70d22e09
菘菘 <rrt467778@gmail.com>
2021-07-06 15:23:40 +0800

    引索檔案

--- 1.16/Spider/Module/LangFile.js
+++ 1.16/Spider/Module/LangFile.js
@@ -6,10 +6,11 @@ const {
     stringify
 } = require('comment-json')
 const CopyDir = require('./CopyDir').CopyDir;
+const CurseForgeIndex = require('./CurseForgeIndex').CurseForgeIndex;
 
 async function LangFile(ModID, slug, id, name) {
     if (config.Blacklist_modId.includes(ModID)) return; //黑名單(模組ID)
-    
+
     if (!fs.existsSync(`${process.cwd()}/../assets/${ModID}`)) {
         fs.mkdirSync(`${process.cwd()}/../assets/${ModID}`);
     }
@@ -17,18 +18,18 @@ async function LangFile(ModID, slug, id, name) {
     /*
     Patchouli 手冊自動添加解析器
     */
-    let PatchouliDir = `${process.cwd()}/../jar/${slug}/data/${ModID}/patchouli_books`
+    let PatchouliDir = `${process.cwd()}/../jar/${slug}/data/${ModID}/patchouli_books/`
     if (fs.existsSync(PatchouliDir)) {
         let DirName = fs.readdirSync(PatchouliDir).toString().split("\n");
         for (let i = 0; i < DirName.length; i++) {
-            let BookDir = `${process.cwd()}/../jar/${slug}/data/${ModID}/patchouli_books/${DirName[i]}/en_us`
+            let BookDir = `${process.cwd()}/../jar/${slug}/data/${ModID}/patchouli_books/${DirName[i]}/en_us`;
             if (fs.existsSync(BookDir)) {
                 if (!fs.existsSync(`${process.cwd()}/../assets/${ModID}/patchouli_books`)) {
                     fs.mkdirSync(`${process.cwd()}/../assets/${ModID}/patchouli_books`);
                     fs.mkdirSync(`${process.cwd()}/../assets/${ModID}/patchouli_books/${DirName[i]}`);
                     fs.mkdirSync(`${process.cwd()}/../assets/${ModID}/patchouli_books/${DirName[i]}/zh_tw`);
                 }
-                CopyDir(BookDir, `${process.cwd()}/../assets/${ModID}/patchouli_books/${DirName[i]}/zh_tw`);
+                await CopyDir(BookDir, `${process.cwd()}/../assets/${ModID}/patchouli_books/${DirName[i]}/zh_tw`);
                 console.log(`複製完成 Patchouli 手冊檔案 (${ModID})`)
             }
         }
@@ -50,9 +51,10 @@ async function LangFile(ModID, slug, id, name) {
     }
     fs.writeFile(`${process.cwd()}/../assets/${ModID}/lang/zh_tw.json`, data, function (error) {
         if (error) {
-            console.log(`解析語系Json檔案時發生錯誤。\n錯誤模組檔案: ${name} (${id}-${ModID})\n錯誤原因: ${error}`);
+            console.log(`寫入語系檔案時發生未知錯誤。\n錯誤模組檔案: ${name} (${id}-${ModID})\n錯誤原因: ${error}`);
         }
-        console.log(`處理 ${name} (${id}-${ModID}) 的原始語系檔案完成`);
+        CurseForgeIndex(ModID, id, name);
+        return console.log(`處理 ${name} (${id}-${ModID}) 的原始語系檔案完成`);
     })
 }
 
