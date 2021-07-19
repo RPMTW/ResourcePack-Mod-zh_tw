794406eeb056496ce8bd19695090030c9a42236b
菘菘 <rrt467778@gmail.com>
2021-07-19 10:17:42 +0800

    version

--- 1.12/Spider/Module/MCVersion.js
+++ 1.12/Spider/Module/MCVersion.js
@@ -1,6 +1,6 @@
 const config = require(`${process.cwd()}/config.json`)
 function MCVersion(data) {
-    return (data.includes(config.ver)) || (data.includes("1.17.1"));
+    return (data.includes(config.ver)) || (data.includes("1.12.1") || (data.includes("1.12")));
 }
 
 exports.MCVersion = MCVersion;
