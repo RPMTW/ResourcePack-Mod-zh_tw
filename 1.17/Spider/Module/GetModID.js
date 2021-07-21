15fcd4b7cdb8ae865a84c956421f7bab690a7f0d
菘菘 <rrt467778@gmail.com>
2021-07-21 19:21:20 +0800

    修改抓取模組ID方式

--- 1.17/Spider/Module/GetModID.js
+++ 1.17/Spider/Module/GetModID.js
@@ -1,40 +1,23 @@
 const fs = require("fs");
-const toml = require('toml');
-const concat = require('concat-stream');
-const {LangFile} = require("./LangFile");
+const { LangFile } = require("./LangFile");
 
 async function GetModID(slug, id, name) {
+    if (String(name) === "undefined") return;
     let ModID;
-    let ForgeCfgPath = `${process.cwd()}/../jar/${slug}/META-INF/mods.toml`;
-    let McModCfgPath = `${process.cwd()}/../jar/${slug}/mcmod.info`;
-    let FabricCfgPath = `${process.cwd()}/../jar/${slug}/fabric.mod.json`;
-    if (fs.existsSync(ForgeCfgPath)) {
-        fs.createReadStream(`${process.cwd()}/../jar/${slug}/META-INF/mods.toml`, 'utf8').pipe(concat(function (data, err) {
-            try {
-                ModID = JSON.parse(JSON.stringify(toml.parse(data))).mods[0].modId
-                LangFile(ModID, slug, id, name);
-            } catch (err) {
-                console.log("解析Toml時發生錯誤 \n" + err)
+    let ModDir = `${process.cwd()}/../jar/${slug}/assets`
+    try {
+        if (fs.existsSync(ModDir)) {
+            let DirName = fs.readdirSync(ModDir).toString().split("\n");
+            for (let i = 0; i < DirName.length; i++) {
+                let ModLangDir = `${process.cwd()}/../jar/${slug}/assets/${DirName[i]}/lang`;
+                if (fs.existsSync(ModLangDir)) {
+                    ModID = DirName[i];
+                    LangFile(ModID, slug, id, name);
+                }
             }
-        }));
-    } else if (fs.existsSync(McModCfgPath)) {
-        fs.createReadStream(`${process.cwd()}/../jar/${slug}/mcmod.info`, 'utf8').pipe(concat(function (data, err) {
-            try {
-                ModID = JSON.parse(data)[0].modid;
-                LangFile(ModID, slug, id, name);
-            } catch (err) {
-                console.log("解析Json時發生錯誤 \n" + err)
-            }
-        }));
-    } else if (fs.existsSync(FabricCfgPath)) {
-        fs.createReadStream(`${process.cwd()}/../jar/${slug}/fabric.mod.json`, 'utf8').pipe(concat(function (data, err) {
-            try {
-                ModID = JSON.parse(data).id;
-                LangFile(ModID, slug, id, name);
-            } catch (err) {
-                console.log("解析Json時發生錯誤 \n" + err)
-            }
-        }));
+        }
+    } catch (err) {
+        console.log(`解析模組ID時發生錯誤 (${name}) \n ${err}`);
     }
 }
 
