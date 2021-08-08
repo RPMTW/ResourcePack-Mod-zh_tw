62d2905de9f2efd0f256df42191197fc5d1c0bb1
菘菘 <rrt467778@gmail.com>
2021-08-08 11:31:49 +0800

    新增一些模組的語系檔案

--- 1.16/Spider/DownloadMod.js
+++ 1.16/Spider/DownloadMod.js
@@ -28,23 +28,22 @@ function Run() {
         files = files.reverse();
         files.sort(function (a, b) {
             return Date.parse(b.timestamp) - Date.parse(a.timestamp);
-          });
+        });
         for (let i = 0; i < files.length; i++) {
             let data = files[i].minecraft_versions;
             if (data.includes(config.ver) || data.includes("1.16.4") || data.includes("1.16.3") || data.includes("1.16.2") || data.includes("1.16.1") || data.includes("1.16")) {
                 fileID = String(files[i].id);
-                fileName = String(files[i].download_url.split("https://edge.forgecdn.net/files/")[1].split(`${fileID.substr(0, 4)}/${fileID.substr(4, 7)}/`)[1]);
-                if (fileName === "undefined") {
-                    fileName = String(files[i].download_url.split("https://edge.forgecdn.net/files/")[1].split(`${fileID.substr(0, 4)}/${fileID.substr(5, 7)}/`)[1]);
-                }
+                fileName = String(files[i].download_url.split("https://edge.forgecdn.net/files/")[1].replace("/", "").split("/")[1]);
                 let test = path.join(ModDirPath, fileName);
                 slug = fileName.split(".jar")[0];
-                files[i].download(test, true).then(() => {
-                    console.log(`${fileName.split(".jar")[0]} 下載完成。`);
-                    compressing.zip.uncompress(`./mod/${fileName}`, "../jar/" + slug).then(() => GetModID(slug, ID, fileName));
-                }).catch((err) => {
-                    console.log("發生未知錯誤: " + err);
-                });
+                try {
+                    files[i].download(test, true).then(r => {
+                        console.log(`${fileName} 下載完成。`);
+                        compressing.zip.uncompress(`./mod/${fileName}`, "../jar/" + slug).then(() => GetModID(slug, ModList[i].projectID, fileName))
+                    });
+                } catch (err) {
+                    console.log("發生未知錯誤 \n" + err);
+                }
                 break;
             }
         }
