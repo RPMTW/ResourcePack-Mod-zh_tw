e89cea25737f547eede2c2750bb509f8fecbd4eb
菘菘 <rrt467778@gmail.com>
2021-10-08 23:06:38 +0800

    修復錯誤，自動更新語系檔案上線

--- Scripts/bin/Models/ModMetadata.dart
+++ Scripts/bin/Models/ModMetadata.dart
@@ -15,7 +15,7 @@ class ModMetadata {
 
   factory ModMetadata.fromForge112(List<int> bytes) {
     String string = Utf8Decoder(allowMalformed: true).convert(bytes);
-    Map meta = json.decode(string)['modList'][0];
+    Map meta = json.decode(string)[0];
     return ModMetadata(meta['modid'], meta['name'], meta['version']);
   }
 
