477e6d02187870b2e263dcd5552e230c048072ea
菘菘 <rrt467778@gmail.com>
2021-10-09 08:30:51 +0800

    修正計算更新語系檔案的數字

--- Scripts/bin/Actions/UpdateLang.dart
+++ Scripts/bin/Actions/UpdateLang.dart
@@ -6,10 +6,12 @@ class UpdateLang {
 
   static Future<void> run() async {
     Map curseForgeIndex = CurseForgeIndex().map;
+    int _doneCount = 1;
 
     for (String curseID in curseForgeIndex.values) {
       try {
-        print("[ ${(curseForgeIndex.values.toList().indexOf(curseID)) + 1}/${curseForgeIndex.length} ] 更新語系檔案中...");
+        _doneCount++;
+        print("[ $_doneCount/${curseForgeIndex.length} ] 更新語系檔案中...");
         await DownloadModLangFile.run(int.parse(curseID));
       } catch (e) {
         print("[$curseID] 更新語系檔案時發生未知錯誤\n$e");
