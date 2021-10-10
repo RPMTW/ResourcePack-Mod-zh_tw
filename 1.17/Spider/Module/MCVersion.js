81573b4a0a41dc5209618505010d9112175fea5d
菘菘 <rrt467778@gmail.com>
2021-10-10 09:32:45 +0800

    刪除舊腳本

--- 1.17/Spider/Module/MCVersion.js
+++
@@ -1,6 +0,0 @@
-const config = require(`${process.cwd()}/config.json`)
-function MCVersion(data) {
-    return (data.includes(config.ver)) || (data.includes("1.17.1"));
-}
-
-exports.MCVersion = MCVersion;
